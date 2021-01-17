0b9c29108453252a6ce3267ceebb4c19170a1f8d
k1lgor <65338982+k1lgor@users.noreply.github.com>
2021-01-17 14:03:37 +0200

    Delete cinema.py

--- cinema.py
+++
@@ -1,9 +0,0 @@
-project = str(input())
-rows = int(input())
-column = int(input())
-if project == 'Premiere':
-    print(f'{(rows * column * 12):.2f} leva')
-elif project == 'Normal':
-    print(f'{(rows * column * 7.5):.2f} leva')
-elif project == 'Discount':
-    print(f'{(rows * column * 5):.2f} leva')
