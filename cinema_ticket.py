bb2fa1ac9e23c48b102b2d95a7c2590a7b22dde8
k1lgor <65338982+k1lgor@users.noreply.github.com>
2021-01-17 14:06:05 +0200

    Delete cinema_ticket.py

--- cinema_ticket.py
+++
@@ -1,7 +0,0 @@
-a = str(input())
-if a == 'Monday' or a == 'Tuesday' or a == 'Friday':
-    print(12)
-elif a == 'Wednesday' or a == 'Thursday':
-    print(14)
-elif a == 'Saturday' or a == 'Sunday':
-    print(16)
