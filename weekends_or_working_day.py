3e952e416dc8cb03958beedcef96c3f351d6bd31
k1lgor <65338982+k1lgor@users.noreply.github.com>
2021-01-17 14:04:05 +0200

    Delete weekends_or_working_day.py

--- weekends_or_working_day.py
+++
@@ -1,7 +0,0 @@
-a = str(input())
-if a == 'Monday' or a == 'Tuesday' or a == 'Wednesday' or a == 'Thursday' or a == 'Friday':
-    print("Working day")
-elif a == 'Saturday' or a == 'Sunday':
-    print("Weekend")
-else:
-    print("Error")
